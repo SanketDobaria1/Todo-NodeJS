f6b70d7005ebb91b8b157d889662ea9921da82bb
Sanket Dobaria <sanketd1117@gmail.com>
2023-05-21 17:32:26 -0400

    Implement user authorization

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,13 +1,32 @@
-const authorizationMiddleware = (req, res, next) => {
-  // Implement your authorization logic here
-  // For example, check if the user has the necessary permissions
+import Task from "../models/task.js";
 
-  // Assuming you have a property on the user object to check the role or permission
-  if (req.user.role !== "admin") {
-    return res.status(403).render("error", { message: "Access Denied" });
+const authorizationMiddleware = async (req, res, next) => {
+  try {
+    const userId = req.session.userId;
+    const taskId = req.params.taskId;
+
+    // Perform the authorization check based on your requirements
+    // For example, you can check if the current user is the owner of the task
+
+    // Retrieve the task from the database
+    const task = await Task.findById(taskId);
+
+    // Check if the task exists
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    // Check if the current user is the owner of the task
+    if (task.createdBy.toString() !== userId) {
+      return res.status(403).json({ error: 'Unauthorized access' });
+    }
+
+    // User is authorized, proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred while authorizing the user' });
   }
-
-  next();
 };
 
 export default authorizationMiddleware;
