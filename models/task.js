84034f0909168cdac9737502cb481b5827966bbc
Sanket Dobaria <sanketd1117@gmail.com>
2023-05-21 15:30:40 -0400

    Update Task model

--- models/task.js
+++ models/task.js
@@ -1,13 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  completed: { type: Boolean, default: false },
-  dueDate: { type: Date },
-  createdAt: { type: Date, default: Date.now }
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  dueDate: {
+    type: Date,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
 export default Task;
